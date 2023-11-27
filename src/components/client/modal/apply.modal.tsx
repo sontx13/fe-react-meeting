97e5921e2c2bd127f77c90752a7d387ea60d233d
sontx13 <sontx13@gmail.com>
2023-11-27 15:35:06 +0700

    diem danh

--- src/components/client/modal/apply.modal.tsx
+++ src/components/client/modal/apply.modal.tsx
@@ -57,7 +57,7 @@ const ApplyModal = (props: IProps) => {
         async customRequest({ file, onSuccess, onError }: any) {
             const res = await callUploadSingleFile(file, "resume");
             if (res && res.data) {
-                setUrlCV(res.data.fileName);
+                setUrlCV(res.data.toString());
                 if (onSuccess) onSuccess('ok')
             } else {
                 if (onError) {
